32addb4c45a52958ff5ded32061be39f525e12ab
Caio Cezar <caiocezartg@gmail.com>
2021-09-18 20:41:44 -0300

    Add Login and Register Pages Function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import { UserStorage } from "./contexts/UserContext";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
-}
+  return (
+    <BrowserRouter>
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </UserStorage>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
